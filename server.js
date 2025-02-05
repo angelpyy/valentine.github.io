912ba96791053f3e222696097357484b769ccb6f
angel <eduardo.angel5970@gmail.com>
2025-02-04 21:36:25 -0600

    added potential server for backend repsonse (not tested)

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse POST data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from root directory
+app.use(express.static('./'));
+
+// Handle food selections
+app.post('/submit-food', (req, res) => {
+  console.log('Food selections:', req.body.food);
+  res.redirect('/dessert.html');
+});
+
+// Handle dessert selections
+app.post('/submit-dessert', (req, res) => {
+  console.log('Dessert selections:', req.body.dessert);
+  res.redirect('/activities.html');
+});
+
+// Handle activity selections
+app.post('/submit-activities', (req, res) => {
+  console.log('Activity selections:', req.body.activities);
+  res.redirect('/lastpage.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
